e6c5c38f195e5fe4cc81e5318a547c40b8cf25ee
Eugene Tulika <vranen@gmail.com>
2016-09-22 18:36:04 -0500

    MAGETWO-57695 Investigate performance of shared file system
    - disable Unison synchronisation for Windows
    - block m2init command

--- Dockerfile
+++ Dockerfile
@@ -83,6 +83,10 @@ RUN chmod +x /usr/local/bin/entrypoint.sh
 ENV PATH /home/magento2/scripts/:/home/magento2/.magento-cloud/bin:$PATH
 ENV PATH /var/www/magento2/bin/magento:$PATH
 
+ENV USE_SHARED_WEBROOT=1
+ENV SHARED_CODE_PATH="/var/www/magento2"
+ENV WEBROOT_PATH="/var/www/magento2"
+
 # Initial scripts
 COPY scripts/ /home/magento2/scripts/
 RUN cd /home/magento2/scripts && composer install
--- conf/entrypoint.sh
+++ conf/entrypoint.sh
@@ -1,21 +1,31 @@
 #!/usr/bin/env bash
 
-echo "[IN PROGRESS] Sync Started." > /var/www/magento2/status.html
-sed -i 's/^\(\s*DirectoryIndex\s*\).*$/\1status.html/' /etc/apache2/sites-enabled/apache-default.conf
-service apache2 restart
+if [ -n $USE_SHARED_WEBROOT ]
+then
+    if [ $USE_SHARED_WEBROOT == "0" ]
+    then
+        echo "[IN PROGRESS] Sync Started." > /var/www/magento2/status.html
+        sed -i 's/^\(\s*DirectoryIndex\s*\).*$/\1status.html/' /etc/apache2/sites-enabled/apache-default.conf
+        service apache2 restart
+        echo "[IN PROGRESS] Sync Started. Copying started" > /var/www/magento2/status.html
+        cp -rf /home/magento2/magento2 /var/www
+        chown -R magento2:magento2 /var/www/magento2
+        chown -R magento2:magento2 /home/magento2/magento2
+        echo "[IN PROGRESS] Copying Finished" > /var/www/magento2/status.html
 
-echo "[IN PROGRESS] Sync Started. Copying started" > /var/www/magento2/status.html
-cp -rf /home/magento2/magento2 /var/www
-chown -R magento2:magento2 /var/www/magento2
-chown -R magento2:magento2 /home/magento2/magento2
-echo "[IN PROGRESS] Copying Finished" > /var/www/magento2/status.html
+        echo "[IN PROGRESS] Unison sync started" > /var/www/magento2/status.html
+        unison magento2
 
-echo "[IN PROGRESS] Unison sync started" > /var/www/magento2/status.html
-unison magento2
-
-echo "[DONE] Sync Finished" > /var/www/magento2/status.html
-sed -i 's/^\(\s*DirectoryIndex\s*\).*$/\1index.html/' /etc/apache2/sites-enabled/apache-default.conf
-service apache2 restart
-rm -rf /var/www/magento2/status.html
+        echo "[DONE] Sync Finished" > /var/www/magento2/status.html
+        sed -i 's/^\(\s*DirectoryIndex\s*\).*$/\1index.html/' /etc/apache2/sites-enabled/apache-default.conf
+        service apache2 restart
+        rm -rf /var/www/magento2/status.html
+        cat >/etc/supervisor/conf.d/unison.conf <<EOL
+[program:unison]
+command = /usr/local/bin/unison.sh
+redirect_stderr = true
+EOL
+    fi
+fi
 
 supervisord -n -c /etc/supervisord.conf
