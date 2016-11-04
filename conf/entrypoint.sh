23a179599db48c86ade78b0296ec07ae18a50ee7
Viktor Paladiychuk <vpaladiychuk@magento.com>
2016-11-04 15:55:36 +0200

    MAGETWO-60212: Unstable execution when starting new container

--- conf/entrypoint.sh
+++ conf/entrypoint.sh
@@ -12,11 +12,11 @@ then
             sed -i 's/^\(\s*DirectoryIndex\s*\).*$/\1status.html/' /home/magento2/magento2/.htaccess
             cp /home/magento2/magento2/.htacces /var/www/magento2/
             chown magento2:magento2 /var/www/magento2/.htaccess
-            service apache2 restart
+            service apache2 start
 
             if [ -n $CREATE_SYMLINK_EE ]
             then
-                if [ $CREATE_SYMLINK_EE = "1" ]
+                if [ $CREATE_SYMLINK_EE == "1" ]
                 then
                     mkdir -p $HOST_CE_PATH
                     ln -s /var/www/magento2/$EE_DIRNAME $HOST_CE_PATH/$EE_DIRNAME
@@ -25,31 +25,20 @@ then
 
             echo "[IN PROGRESS] Unison sync started" > /var/www/magento2/status.html
 
-            su - magento2 -c "unison magento2 >> /home/magento2/custom_unison.log"
-
-            if [ $? != 0 ]
-            then
-                su - magento2 -c "unison magento2 >> /home/magento2/custom_unison.log"
-            fi
+            (su - magento2 -c 'unison magento2') || (su - magento2 -c 'unison magento2')
 
             chmod +x /var/www/magento2/bin/magento
 
             echo "[DONE] Sync Finished" > /var/www/magento2/status.html
             sed -i 's/^\(\s*DirectoryIndex\s*\).*$/\1index.php/' /home/magento2/magento2/.htaccess
             sed -i 's/^\(\s*DirectoryIndex\s*\).*$/\1index.php/' /var/www/magento2/.htaccess
-            service apache2 restart
             rm -rf /var/www/magento2/status.html
             rm -rf /home/magento2/magento2/status.html
+
+            su - magento2 -c 'unison -repeat=watch magento2' &
         else
-            su - magento2 -c "unison magento2 >> /home/magento2/custom_unison.log"
-
-            if [ $? != 0 ]
-            then
-                su - magento2 -c "unison magento2 >> /home/magento2/custom_unison.log"
-            fi
+            ((su - magento2 -c 'unison magento2') || (su - magento2 -c 'unison magento2'); (su - magento2 -c 'unison -repeat=watch magento2')) &
         fi
-
-       su - magento2 -c "unison -repeat=watch magento2 > /home/magento2/custom_unison.log 2>&1 &"
     fi
 fi
 
